a21a6c37073c01a62120049841bef44ce3a52341
Zekrom123 <fachri21@mhs.unsyiah.ac.id>
2023-04-05 03:02:34 +0700

    View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('createThread');
+});
+
+Route::get('/listSThread', function () {
+    return view('listThread');
 });
