882c79d948adf46f3a7c79ab5bc13004506e0a2a
Ahmad Naziel Firdaus <ahmad9@mhs.unsyiah.ac.id>
2023-05-18 13:39:36 +0700

    landing

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\anime;
+use App\Models\character;
+use App\Models\thread;
 
 class LandingPageController extends Controller
 {
     public function index(){
-        return view('landingpage', [
-           'title' => 'Wibu.com'
+
+        $anime = anime::all();
+        $karakter = character::all();
+        $thread = thread::all();
+        return view('landingpage',[
+           'title' => 'Wibu.com', compact('anime','karakter','thread')
       ]);
     }
 }
